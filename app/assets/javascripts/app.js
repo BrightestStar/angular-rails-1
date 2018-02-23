962e4ea3f28fe2765b1bc4c47507db493720cdfb
lucas <d2194499406@gmail.com>
2018-02-23 20:00:51 +0800

    Getting started with Rails and angular

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,20 @@
+angular.module('flapperNews', ['ui.router', 'templates'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+'$locationProvider',
+function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: 'posts/_posts.html',
+      controller: 'PostsCtrl'
+    });
+
+}])
